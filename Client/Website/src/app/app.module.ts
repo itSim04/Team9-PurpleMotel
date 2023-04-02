c6af57e7b6bdecbf8e7c3ecd02f7e280be36d42b
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-02 20:16:23 +0300

    Added some modules
    1. Construct basic components and services
    2. 10
    3. Some module will be essential throughout the project, including translation.

--- Client/Website/src/app/app.module.ts
+++ Client/Website/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+
+  return new TranslateHttpLoader(http,
+    './assets/i18n/',
+    '.json');
+
+}
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({ defaultLanguage: 'en', loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient], }, }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
