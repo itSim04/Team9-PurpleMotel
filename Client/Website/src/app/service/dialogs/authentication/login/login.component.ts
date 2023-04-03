8211b4370094f042be2d4d2be524bc8584c25372
Charbellgg <charbelgerges40@gmail.com>
2023-04-03 12:30:55 +0300

    Added Functionality to the Login Dialog🔒
    1. Complete Authentication
    2. 10
    3. Retrieved and validated the email. Also wrapped the styling of the forgot password in a class.

--- Client/Website/src/app/service/dialogs/authentication/login/login.component.ts
+++ Client/Website/src/app/service/dialogs/authentication/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login-dialog',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email = "";
+  password = "";
+  validated_email = true;
+  constructor () { }
+
+  validateEmail() {
+    const expression = /^[A-Za-z](\w|\.|_)*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    const result: boolean = expression.test(this.email); // true
+
+    return result;
+  }
 
 }
+
