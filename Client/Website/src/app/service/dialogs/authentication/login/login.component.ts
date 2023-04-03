1115478d90695d651a474664ea7a4929d71a8a7f
Charbellgg <charbelgerges40@gmail.com>
2023-04-03 12:45:57 +0300

    Created the Login Function🔒
    1. Complete Authentication
    2. 10
    3. Created the Login function logic without the api

--- Client/Website/src/app/service/dialogs/authentication/login/login.component.ts
+++ Client/Website/src/app/service/dialogs/authentication/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,12 +10,23 @@ export class LoginComponent {
   email = "";
   password = "";
   validated_email = true;
-  constructor () { }
+  constructor (private http: HttpClient) {}
+
+
+  login() {
+
+    this.validated_email = this.validateEmail();
+    
+    if (this.validated_email) {
+
+      //call to the api
+    }
+  }
 
   validateEmail() {
     const expression = /^[A-Za-z](\w|\.|_)*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
-    const result: boolean = expression.test(this.email); // true
+    const result: boolean = expression.test(this.email);
 
     return result;
   }
