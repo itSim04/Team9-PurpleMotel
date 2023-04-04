6cc2a943bb6d1fb3748dd791de3fdc518061e54e
Charbellgg <charbelgerges40@gmail.com>
2023-04-04 12:15:06 +0300

    Created the Login function🔒
    1. Complete Authentication
    2. 10
    3. Created the Login function that will authenticate the user. Included JWT for security reasons.

---
+++ Server/app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class AuthenticationController extends Controller
+{
+    public function login(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+
+            return response()->json([
+
+                'status' => 'error',
+                'message' => 'Unauthorized',
+
+            ], 401);
+            
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'data' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+
+    }
+}
--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->controller(UserController::class)->group(function () {
+    Route::prefix('auth')->controller(AuthenticationController::class)->group(function () {
 
         Route::post('login', 'login');
         Route::post('register', 'register');
