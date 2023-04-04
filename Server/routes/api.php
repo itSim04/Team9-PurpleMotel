ddfdd682ed22becb459670839d4fb34357f89f0e
Charbellgg <charbelgerges40@gmail.com>
2023-04-04 11:34:48 +0300

    Routed the Authentication APIs🔒
    1. Complete Authentication
    2. 10
    3. Established the API routes for the authentication.

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->controller(UserController::class)->group(function () {
+
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    });
 });
