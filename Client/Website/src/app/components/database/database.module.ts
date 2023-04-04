27a785e233ad72ea1dee1a71044bb425fa4f707a
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-05 00:58:03 +0300

    Created the basic design of the filter
    1. Construct basic components and services
    2. 10
    3. Prepared the filter component with a design that handles text based content

--- Client/Website/src/app/components/database/database.module.ts
+++ Client/Website/src/app/components/database/database.module.ts
@@ -1,12 +1,32 @@
+import { LanguageModule } from './../../service/language/language.module';
+import { DatabaseComponent } from './database.component';
+import { TableUnitComponent } from './blocks/table-unit/table-unit.component';
+import { TableComponent } from './blocks/table/table.component';
+import { FilterComponent } from './blocks/filter/filter.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+    FilterComponent, 
+    TableComponent, 
+    TableUnitComponent, 
+    DatabaseComponent
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatOptionModule,
+    LanguageModule
+
   ]
 })
 export class DatabaseModule { }
