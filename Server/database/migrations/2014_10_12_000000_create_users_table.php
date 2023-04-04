10626855b2e9150278a276564ceecfb1889c669f
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-04 09:49:02 +0300

    Created the User migration
    1. Engineer and implement database
    2. 10
    3. Created the User migration that will be reflected as a table in the database.

--- Server/database/migrations/2014_10_12_000000_create_users_table.php
+++ Server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('country');
+            $table->string('tier');
+            $table->tinyInteger('gender');
+            $table->string('date_of_birth');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
