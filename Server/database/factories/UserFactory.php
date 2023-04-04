be4cb3c41a455093db7f214a851cdf6c09f9c28e
Charbellgg <charbelgerges40@gmail.com>
2023-04-04 12:29:34 +0300

    Created the User Seeder🔒
    1. Complete Authentication
    2. 10
    3. Created the user seeder that will call the factory that will serve as dummy data.

--- Server/database/factories/UserFactory.php
+++ Server/database/factories/UserFactory.php
@@ -18,14 +18,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'gender' => random_int(0, 3),
+            'date_of_birth' => fake()->date(),
+            'country' => fake()->country(),
+            'tier' => random_int(1, 10),
+            'phone' => fake()->unique()->phoneNumber(),
+            'language' => random_int(1, 10),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      */
