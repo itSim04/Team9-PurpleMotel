7fbb2e46c931deba63e3bf5b42e7ca6d04265f50
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-05 10:50:43 +0300

    Designed the Database component
    1. Construct basic components and services
    2. 10
    3. Designed the database component that will combine what we already created earlier. Added some spare attributes to the unit

--- Client/Website/src/app/components/database/blocks/table-unit/table-unit.component.ts
+++ Client/Website/src/app/components/database/blocks/table-unit/table-unit.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { DataInjection } from 'src/app/models/Database';
+import { ChangeInjection, DataInjection } from 'src/app/models/Database';
 import { Required } from '../../database.component';
 
 @Component({
@@ -11,13 +11,16 @@ import { Required } from '../../database.component';
 })
 export class TableUnitComponent<Data, Data2> {
 
-  @Input() @Required  data: [string, Data][] = [];
-  @Input() @Required  data_injection!: DataInjection<Data>;
-  
-  @Input() @Required  filtered_data!: MatTableDataSource<[string, Data], MatPaginator>;
-  @Input() @Required  extra_data: [string, Data2][] | undefined = [];
-  
-  @Output() modify_click: EventEmitter<[string, Data]> = new EventEmitter();
+  @Input() @Required data: [string, Data][] = [];
+  @Input() @Required data_map: Map<string, Data> = new Map();
+  @Input() @Required data_injection!: DataInjection<Data>;
+
+  @Input() @Required change_injection?: ChangeInjection<Data>;
+
+  @Input() @Required filtered_data!: MatTableDataSource<[string, Data], MatPaginator>;
+  @Input() @Required extra_data: [string, Data2][] | undefined = [];
+  @Input() @Required extra_data_map: Map<string, Data2> | undefined = new Map();
+
   @Output() hover: EventEmitter<[string, Data | undefined]> = new EventEmitter();
 
 }
