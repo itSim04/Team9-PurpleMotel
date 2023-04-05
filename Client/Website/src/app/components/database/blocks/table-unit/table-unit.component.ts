f0ab74d1705b7fb58a56a89800c768a6eff2f24f
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-05 11:58:47 +0300

    Added a displaying function for the change
    1. Construct basic components and services
    2. 10
    3. Added displaying functionality to the change system

--- Client/Website/src/app/components/database/blocks/table-unit/table-unit.component.ts
+++ Client/Website/src/app/components/database/blocks/table-unit/table-unit.component.ts
@@ -1,13 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeComponent } from './../../change/change.component';
+import { ChangeInjection } from './../../../../models/Database';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { ChangeInjection, DataInjection } from 'src/app/models/Database';
+import { DataInjection } from 'src/app/models/Database';
 import { Required } from '../../database.component';
+import { KeyValue } from '@angular/common';
+import { AddDialogControllerService } from 'src/app/service/dialogs/add/add-dialog-controller.service';
 
 @Component({
   selector: 'app-table-unit',
   templateUrl: './table-unit.component.html',
-  styleUrls: ['./table-unit.component.scss'],
+  styleUrls: ['./table-unit.component.scss']
 })
 export class TableUnitComponent<Data, Data2> {
 
@@ -23,6 +27,61 @@ export class TableUnitComponent<Data, Data2> {
 
   @Output() hover: EventEmitter<[string, Data | undefined]> = new EventEmitter();
 
-  
+  constructor (private add_service: AddDialogControllerService) { }
+
+  displayAdd() {
+
+    if (this.change_injection) {
+
+      this.change_injection.affected_data = undefined;
+      const dialogRef = this.add_service.openDialog<Data>(ChangeComponent, this.change_injection, this.extra_data_map);
+      dialogRef.afterClosed().subscribe((result: KeyValue<string, Data>) => {
+
+        if (result) {
+
+          this.data_map.set(result.key, result.value);
+          this.data.push([result.key, result.value]);
+          this.filtered_data.data = this.data;
+
+        }
+      });
+
+    }
+
+  }
+
+  displayModify(data: [string, Data | Data2]) {
+
+
+    if (this.change_injection) {
+
+      this.change_injection.affected_data = { key: data[0], value: data[1] as Data };
+
+      const dialogRef = this.add_service.openDialog<Data>(ChangeComponent, this.change_injection, this.extra_data_map);
+      dialogRef.afterClosed().subscribe(result => {
+
+
+        const index = this.data.findIndex(t => t[0] == data[0]);
+
+        if (result) {
+
+          if (result.value) {
+
+            this.data[index] = ([result.key, result.value]);
+            this.filtered_data.data = this.data;
+
+          } else {
+
+            this.data.splice(index, 1);
+            this.filtered_data.data = this.data;
+
+          }
+        }
+      });
+
+    }
+
+  }
+
 
 }
