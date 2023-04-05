01fa90455e98ea3de9713ee3b80699dd42bb5eb8
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-05 09:56:13 +0300

    Created the basic logic of the Table
    1. Construct basic components and services
    2. 10
    3. Prepared the logic of the table. The table will be used by the database to display the data.

--- Client/Website/src/app/components/database/blocks/table/table.component.ts
+++ Client/Website/src/app/components/database/blocks/table/table.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { formatPrice, formatWord } from '../../database.component';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Data } from '@angular/router';
+import { DataInjection } from 'src/app/models/Database';
+import { formatPrice, formatWord, Required } from '../../database.component';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent  implements OnInit {
+export class TableComponent<Data, Data2> implements AfterViewInit {
 
-  constructor() { }
+  @Input() @Required data: [string, Data][] = [];
+  @Input() @Required data_injection!: DataInjection<Data>;
+  @Input() @Required filtered_data!: MatTableDataSource<[string, Data], MatPaginator>;
+  @Input() @Required extra_data: [string, Data2][] | undefined = [];
 
-  ngOnInit() {}
+  @Output() modify_click: EventEmitter<[string, Data]> = new EventEmitter();
+  @Output() hover: EventEmitter<[string, Data | undefined]> = new EventEmitter();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit(): void {
+
+    this.filtered_data.paginator = this.paginator;
+    this.filtered_data.sort = this.sort;
+
+  }
 
   formatPrice(price: number): string {
 
@@ -23,6 +42,8 @@ export class TableComponent  implements OnInit {
     return formatWord(word);
 
   }
+
+
 
 
 }
