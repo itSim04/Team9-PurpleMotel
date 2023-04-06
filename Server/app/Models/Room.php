2b5da356798c6d140f94a5a33d100ef8a06f6dce
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-06 20:01:50 +0300

    Created the Room Model🛏
    1. Complete Room Database Page
    2. 9
    3. Created the model and the supporting files. Filled the attributes array.

---
+++ Server/app/Models/Room.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Summary of Room
+ */
+class Room extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'label',
+        'description',
+        'number',
+        'level',
+        'type',
+        'is_open',
+        'rating'
+    ];
+    
+}
