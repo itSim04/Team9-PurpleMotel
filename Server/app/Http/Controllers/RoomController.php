f93e45509da284824ca0bfcaacdec2b63a79436c
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-06 21:02:13 +0300

    Populated the Room Controller🛏
    1. Complete Room Database Page
    2. 9
    3. Filled up the room controller

--- Server/app/Http/Controllers/RoomController.php
+++ Server/app/Http/Controllers/RoomController.php
@@ -3,64 +3,70 @@
 namespace App\Http\Controllers;
 
 use App\Models\Room;
+use App\Http\Resources\RoomResource;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreRoomRequest;
 use App\Http\Requests\UpdateRoomRequest;
 
 class RoomController extends Controller
 {
+    protected $options = [
+
+        'level' => 'required|numeric|between:-1000,1000',
+        'number' => 'required|numeric|between:0,1000',
+        'type' => 'required|numeric|integer|between:1,1000',
+        'open' => 'boolean',
+        'rating' => 'numeric|integer:between:0,50',
+        'label' => 'required|string',
+        'description' => 'string'
+
+    ];
+    protected $resource = RoomResource::class;
+    protected $model = Room::class;
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        // return JWTAuth::parseToken()->authenticate();
+        return indexTemplate($this->model, $this->resource);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRoomRequest $request)
+    public function store(Request $request)
     {
-        //
+
+        return storeTemplate($request, $this->model, $this->resource, $this->options);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Room $room)
+    public function show(int $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Room $room)
-    {
-        //
+        return showTemplate($this->model, $this->resource, $id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRoomRequest $request, Room $room)
+    public function update(Request $request, string $room_id)
     {
-        //
+
+        return updateTemplate($request, $this->model, $room_id, $this->resource, $this->options);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Room $room)
+    public function destroy(int $id)
     {
-        //
+
+        return destroyTemplate($this->model, $id);
+
     }
 }
