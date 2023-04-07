25a32fd9a2122905abf33993503d0c1ff6c54a0e
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-07 23:27:38 +0300

    Populated the Food Controller🍴
    1. Complete Food Database Page
    2. 10
    3. Filled up the food controller

--- Server/app/Http/Controllers/FoodController.php
+++ Server/app/Http/Controllers/FoodController.php
@@ -5,62 +5,63 @@ namespace App\Http\Controllers;
 use App\Models\Food;
 use App\Http\Requests\StoreFoodRequest;
 use App\Http\Requests\UpdateFoodRequest;
+use App\Http\Resources\FoodResource;
+use Illuminate\Http\Request;
 
 class FoodController extends Controller
 {
+    protected $resource = FoodResource::class;
+    protected $model = Food::class;
+    protected $model_name = 'Foods';
+    protected $options = [
+
+        'label' => 'required|string|max:64',
+        'description'=> 'string|max:255',
+        'price' => 'required|numeric|min:0',
+        'is_served' => 'required|boolean'
+
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return indexTemplate($this->model, $this->resource);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreFoodRequest $request)
+    public function store(Request $request)
     {
-        //
+        return storeTemplate($request, $this->model, $this->resource, $this->options);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Food $food)
+    public function show(int $id)
     {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Food $food)
-    {
-        //
+        return showTemplate($this->model, $this->resource, $id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateFoodRequest $request, Food $food)
+    public function update(Request $request, string $food_id)
     {
-        //
+       
+        return updateTemplate($request, $this->model, $food_id, $this->resource, $this->options, $this->model_name);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Food $food)
+    public function destroy(int $id)
     {
-        //
+
+        return destroyTemplate($this->model, $id);
     }
 }
