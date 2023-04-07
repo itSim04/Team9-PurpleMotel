c080bf4b11037fbbb094bb90ddc85f17122c8324
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-08 00:21:54 +0300

    Added the Food Seeder🍴
    1. Complete Food Database Page
    2. 10
    3. Filled up the seeder.

--- Server/database/seeders/FoodSeeder.php
+++ Server/database/seeders/FoodSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Food;
 class FoodSeeder extends Seeder
 {
     /**
@@ -12,6 +12,6 @@ class FoodSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Food::factory(20)->create();
     }
 }
