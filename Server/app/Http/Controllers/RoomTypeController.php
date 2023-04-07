60cef4ec36936c3da499f51be38e56e67bbb13a6
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-07 18:34:13 +0300

    Populated the RoomType Controller🛏
    1. Completed RoomTypes Database Page
    2. 10
    3. Populated the RoomType controller functions that will handle corresponding incoming HTTP requests.

--- Server/app/Http/Controllers/RoomTypeController.php
+++ Server/app/Http/Controllers/RoomTypeController.php
@@ -3,64 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Models\RoomType;
+use App\Http\Resources\RoomTypeResource;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreRoomTypeRequest;
 use App\Http\Requests\UpdateRoomTypeRequest;
 
 class RoomTypeController extends Controller
 {
+
+    protected $resource = RoomTypeResource::class;
+    protected $model = RoomType::class;
+
+    protected $options = [
+
+        'label' => 'required|string|max:32',
+        'price' => 'required|numeric',
+        'adults_capacity' => 'required|integer|min:0',
+        'adults_with_kids_capacity' => 'required|integer|min:0',
+        'kids_capacity' => 'required|integer|min:0'
+
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return indexTemplate($this->model, $this->resource);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRoomTypeRequest $request)
+    public function store(Request $request)
     {
-        //
+
+        return storeTemplate($request, $this->model, $this->resource, $this->options);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(RoomType $roomType)
+    public function show(int $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(RoomType $roomType)
-    {
-        //
+        return showTemplate($this->model, $this->resource, $id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRoomTypeRequest $request, RoomType $roomType)
+    public function update(Request $request, string $roomType_id)
     {
-        //
+
+        return updateTemplate($request, $this->model, $roomType_id, $this->resource, $this->options);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(RoomType $roomType)
+    public function destroy(int $id)
     {
-        //
+
+        return destroyTemplate($this->model, $id);
     }
 }
