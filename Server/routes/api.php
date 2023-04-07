e65dbc735afba7175805f792e143d2e5a27538af
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-07 17:02:56 +0300

    Updated api.php🛏
    1. Completed Room Database Page
    2. 10
    3. Added the missing import to api.php

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Password;
