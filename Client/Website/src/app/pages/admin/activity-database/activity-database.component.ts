36e0607dbeb203db7de798b2eae46bf7af79c63a
Charbellgg <charbelgerges40@gmail.com>
2023-04-08 21:42:34 +0300

    Created the Activities Extra Injection🏃
    1. Complete Activity Database Page
    2. 9
    3. Created the activities extra injection that will fill the activities table in the activities db page.

--- Client/Website/src/app/pages/admin/activity-database/activity-database.component.ts
+++ Client/Website/src/app/pages/admin/activity-database/activity-database.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ChangeInjection, DataInjection } from 'src/app/models/Database';
+import { Activity } from 'src/app/models/Activity';
+import { ActivityDatabaseService } from './activity-database.service';
+import { map } from 'rxjs';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-activity-database',
@@ -6,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./activity-database.component.scss']
 })
 export class ActivityDatabaseComponent {
+
+  extra_injection: DataInjection<Activity> = {
+
+    title: 'Activity',
+    displayed_columns: [
+      {
+        key:'title'
+      },
+      {
+        key:'description'
+      },
+      {
+        key:'price'
+      },
+      {
+        key:'capacity'
+      },
+      {
+        key:'start_date'
+      },
+      {
+        key:'end_date'
+      },
+
+    ],
+    data_fetcher:()=>this.activity_service.getAllActivities().pipe(map(data => data.activities))
+  }
+  
+  constructor(private activity_service: ActivityDatabaseService){}
+
+  
 
 }
