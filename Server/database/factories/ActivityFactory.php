1d2b0f463079bff0a1bdcdc55661a27191c86de3
Charbellgg <charbelgerges40@gmail.com>
2023-04-08 14:14:42 +0300

    Created and Populated the Activity Factory🏃
    1. Complete Activity Database Page
    2. 10
    3. Filled the activity factory with conditions set for the dummy data that will be generated later on.

--- Server/database/factories/ActivityFactory.php
+++ Server/database/factories/ActivityFactory.php
@@ -17,7 +17,12 @@ class ActivityFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->firstName(),
+            'description' => fake()->sentence(),
+            'capacity' => random_int(0,64),
+            'price' => random_int(0,255),
+            'start_date' => fake()->date(),
+            'end_date' => fake()->date()
         ];
     }
 }
