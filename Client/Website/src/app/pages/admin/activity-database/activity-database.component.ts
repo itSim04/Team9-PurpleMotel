d346e71f2bb4ca45e8b2f18d54d2c35f04d2fe42
Charbellgg <charbelgerges40@gmail.com>
2023-04-08 21:54:04 +0300

    Created and Populated the Activities Extra Change Injection🏃
    1. Complete Activity Database Page
    2. 10
    3. Created and tested the activities extra change injection that will allow the admin to modify, add and remove activities from the database.

--- Client/Website/src/app/pages/admin/activity-database/activity-database.component.ts
+++ Client/Website/src/app/pages/admin/activity-database/activity-database.component.ts
@@ -41,6 +41,50 @@ export class ActivityDatabaseComponent {
   
   constructor(private activity_service: ActivityDatabaseService){}
 
-  
+  extra_change_injection: ChangeInjection<Activity> = {
+    default_state: {
+      title: '',
+      description: '',
+      price: 0,
+      capacity: 0,
+      start_date: new Date(),
+      end_date: new Date()
+    },
+
+    data_type: 'activity',
+
+    fields: [
+      {
+        key: 'title',
+        type: 'text'
+      },
+      {
+        key: 'description',
+        type: 'text'
+      },
+      {
+        key: 'capacity',
+        type: 'number'
+      },
+      {
+        key: 'price',
+        type: 'number'
+      },
+      {
+        key: 'start_date',
+        type: 'date'
+      },
+      {
+        key: 'end_date',
+        type: 'date'
+      }
+    ],
+
+    add_service: activity => this.activity_service.addNewActivity(activity),
+    modify_service: (key, data) => this.activity_service.modifyActivity(key, data),
+    delete_service: key => this.activity_service.deleteActivity(key),
+    identifier: (data) => '' + data.title,
+    side_panel: 'empty'
+  }
 
 }
