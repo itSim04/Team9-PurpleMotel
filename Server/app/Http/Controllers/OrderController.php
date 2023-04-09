a419abad738012143b4daba459ccb4ce982ba8ba
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-09 22:17:33 +0300

    Populated the Order Controller🧾
    1. Complete Order Database Page
    2. 10
    3. Filled up the order controller.

--- Server/app/Http/Controllers/OrderController.php
+++ Server/app/Http/Controllers/OrderController.php
@@ -5,62 +5,61 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
+use App\Http\Resources\OrderResource;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    protected $resource = OrderResource::class;
+    protected $model = Order::class;
+    protected $model_name = 'Orders';
+    protected $options = [
+
+        'date' => 'required|date',
+        'status'=> 'required|string'
+
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return indexTemplate($this->model, $this->resource);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreOrderRequest $request)
+    public function store(Request $request)
     {
-        //
+        return storeTemplate($request, $this->model, $this->resource, $this->options);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Order $order)
+    public function show(int $id)
     {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Order $order)
-    {
-        //
+        return showTemplate($this->model, $this->resource, $id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateOrderRequest $request, Order $order)
+    public function update(Request $request, string $order_id)
     {
-        //
+       
+        return updateTemplate($request, $this->model, $order_id, $this->resource, $this->options, $this->model_name);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Order $order)
+    public function destroy(int $id)
     {
-        //
+
+        return destroyTemplate($this->model, $id);
     }
 }
