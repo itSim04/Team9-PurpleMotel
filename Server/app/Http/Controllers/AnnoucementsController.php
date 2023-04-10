a1c9e5dd2b641e8e753f8201cfba642fdb6f9297
Daniel Khater <formaldk737@gmail.com>
2023-04-10 21:51:44 +0300

    Created the Announcements Model📢
    1. Complete Announcements Database Page
    2. 10
    3. Created the model and the supporting files. Filled the attributes.

---
+++ Server/app/Http/Controllers/AnnoucementsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Annoucements;
+use App\Http\Requests\StoreAnnoucementsRequest;
+use App\Http\Requests\UpdateAnnoucementsRequest;
+
+class AnnoucementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAnnoucementsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Annoucements $annoucements)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Annoucements $annoucements)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnnoucementsRequest $request, Annoucements $annoucements)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Annoucements $annoucements)
+    {
+        //
+    }
+}
