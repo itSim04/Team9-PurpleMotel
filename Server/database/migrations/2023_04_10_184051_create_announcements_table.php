957f9f083bbb46876d0be0539c3a77b788af6a15
Daniel Khater <formaldk737@gmail.com>
2023-04-10 23:07:31 +0300

    Structured the Migration Tables📢
    1. Complete Announcements Database Page
    2. 9
    3. Created the tables for the migration.

--- Server/database/migrations/2023_04_10_184051_create_announcements_table.php
+++ Server/database/migrations/2023_04_10_184051_create_announcements_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
+            $table->string('label');
+            $table->string('body');
             $table->timestamps();
         });
     }
