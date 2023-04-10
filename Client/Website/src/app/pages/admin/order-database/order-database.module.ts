fd94be807d7ba570d4c3e1330348f406347945c9
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-11 01:21:34 +0300

    Completed the Linking🧾
    1. Complete Order Database Page
    2. 10
    3.  Completed the linking process.

--- Client/Website/src/app/pages/admin/order-database/order-database.module.ts
+++ Client/Website/src/app/pages/admin/order-database/order-database.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderDatabaseComponent } from './order-database.component';
+import { DatabaseModule } from 'src/app/components/database/database.module';
+import { forwardRef } from "@angular/core";
 
 
 
 @NgModule({
-  declarations: [
-    OrderDatabaseComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    OrderDatabaseComponent
-  ]
+    declarations: [
+        OrderDatabaseComponent
+    ],
+    exports: [
+        OrderDatabaseComponent,
+        DatabaseModule
+    ],
+    imports: [
+        CommonModule
+    ]
 })
 export class OrderDatabaseModule { }
