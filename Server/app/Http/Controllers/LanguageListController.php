fa9a8d87766501d28b8396248ea34450c1869194
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-10 13:12:47 +0300

    Created the Models🌐
    1. Complete Languages Database Page
    2. 10
    3. Created the model and the supporting files.

---
+++ Server/app/Http/Controllers/LanguageListController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LanguageList;
+use App\Http\Requests\StoreLanguageListRequest;
+use App\Http\Requests\UpdateLanguageListRequest;
+
+class LanguageListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLanguageListRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LanguageList $languageList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LanguageList $languageList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLanguageListRequest $request, LanguageList $languageList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LanguageList $languageList)
+    {
+        //
+    }
+}
