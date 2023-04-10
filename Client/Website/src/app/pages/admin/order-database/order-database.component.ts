045ec3143cee13e0a33d450efe4963f9a74aa5cb
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-11 01:16:12 +0300

    Added the Order Change Injection🧾
    1. Complete Order Database Page
    2. 10
    3. Added the change injection that will let the admin manage the orders database.

--- Client/Website/src/app/pages/admin/order-database/order-database.component.ts
+++ Client/Website/src/app/pages/admin/order-database/order-database.component.ts
@@ -56,4 +56,46 @@ export class OrderDatabaseComponent {
     }))
 
   };
+
+  change_injection: ChangeInjection<Order> = {
+    side_panel: 'empty',
+    default_state: {
+      date: new Date(),
+      status: "",
+      food_id: '0',
+      user_id: '0'
+    },
+    data_type: 'Order',
+    fields: [
+      {
+        key: 'date',
+        type: 'date'
+      },
+      {
+        key: 'user_id',
+        type: 'outer_selection',
+        outer_choices: {
+
+          format: (choice) => (choice as User)?.first_name + ' ' + (choice as User)?.last_name,
+          index: 3
+        }
+      },
+      {
+        key: 'food_id',
+        type: 'outer_selection',
+        outer_choices: {
+
+          format: (choice) => {
+
+            return (choice as Food)?.label;
+          },
+          index: 1
+        }
+      }
+    ],
+    add_service: order => this.order_service.addNewOrder(order),
+    modify_service: (key, data) => this.order_service.modifyOrder(key, data),
+    delete_service: key => this.order_service.deleteOrder(key),
+    identifier: (data) => data.date + " "
+  };
 }
