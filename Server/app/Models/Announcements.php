ec20794427950ca41795d9d8f43ebfbbb5965a99
Daniel Khater <formaldk737@gmail.com>
2023-04-10 23:06:15 +0300

    Amended Grammatical Errors📢
    1. Complete Announcements Database Page
    2. 8
    3. Fixed grammatical mistakes where I wrote 'annoucement' in stead of 'announcement'.

--- Server/app/Models/Announcements.php
+++ Server/app/Models/Announcements.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Annoucements extends Model
+class Announcements extends Model
 {
     use HasFactory;
     protected $fillable = [
--- Server/database/seeders/AnnouncementsSeeder.php
+++ Server/database/seeders/AnnouncementsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AnnoucementsSeeder extends Seeder
+class AnnouncementsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
