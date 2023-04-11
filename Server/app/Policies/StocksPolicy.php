e6fee31a5515f83c3a12017b1100d0ea1d311d46
Charbellgg <charbelgerges40@gmail.com>
2023-04-11 09:50:13 +0300

    Populated Stocks Policy🪪
    1. N/A
    2. 10
    3. Filled the stocks policy that mirror the user's permissions.

--- Server/app/Policies/StocksPolicy.php
+++ Server/app/Policies/StocksPolicy.php
@@ -2,65 +2,102 @@
 
 namespace App\Policies;
 
-use App\Models\Stocks;
-use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use App\Models\UserType;
+use Illuminate\Foundation\Auth\User;
 
-class StocksPolicy
+class UserTypePolicy
 {
+
+    private $permission_name = 'stock';
+
     /**
-     * Determine whether the user can view any models.
+     * Create a new policy instance.
      */
-    public function viewAny(User $user): bool
+    public function __construct()
     {
         //
     }
 
     /**
      * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
      */
-    public function view(User $user, Stocks $stocks): bool
+    public function viewAny(User $user)
     {
-        //
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
+
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][0];
+
+        }
     }
 
     /**
-     * Determine whether the user can create models.
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user_type
+     * @return bool
      */
-    public function create(User $user): bool
+    public function view(User $user): bool
     {
-        //
+
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
+
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][0];
+            
+        }
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Stocks $stocks): bool
-    {
-        //
+    public function update(User $user) {
+
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
+
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][1];
+            
+        }
+
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Stocks $stocks): bool
-    {
-        //
-    }
+    public function delete(User $user) {
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Stocks $stocks): bool
-    {
-        //
-    }
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Stocks $stocks): bool
-    {
-        //
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][2];
+            
+        }
+
     }
 }
