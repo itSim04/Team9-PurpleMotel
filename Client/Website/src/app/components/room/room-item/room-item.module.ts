1a91ebdb8690bde9a14f9e5572dd91f4bf15c316
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-11 11:31:59 +0300

    Designed the Room Component🏡
    1. Design, Implement and Link Rooms Page
    2. 10
    3. Designed the room component that will be used in the guest room page.

---
+++ Client/Website/src/app/components/room/room-item/room-item.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomItemComponent } from './room-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    RoomItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RoomItemModule { }
