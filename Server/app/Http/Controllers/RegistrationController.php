186b2e4bac9164e97e3122f82e5449e976a51d68
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-15 00:18:55 +0300

    Populated the Registration Controller🗄
    1. Complete Registration Database Page
    2. 10
    3. Populated the controller.

--- Server/app/Http/Controllers/RegistrationController.php
+++ Server/app/Http/Controllers/RegistrationController.php
@@ -5,62 +5,72 @@ namespace App\Http\Controllers;
 use App\Models\Registration;
 use App\Http\Requests\StoreRegistrationRequest;
 use App\Http\Requests\UpdateRegistrationRequest;
+use App\Models\User;
+use App\Models\UserType;
+use App\Models\Activity;
+use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
 {
+    protected $resource = RegistrationResource::class;
+    protected $model = Registration::class;
+    protected $model_name = 'Registrations';
+    protected $options = [
+
+        'activity_id' => 'required|string',
+        'user_id' => 'required|string',
+        'start_date' => 'required|date',
+        'end_date' => 'required|date',
+
+
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return indexTemplate($this->model, $this->resource, [
+            
+            User::class => UserResource::class, 
+            UserType::class => UserTypeResource::class, 
+            Activity::class => ActivityResource::class
+        
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRegistrationRequest $request)
+    public function store(Request $request)
     {
-        //
+        return storeTemplate($request, $this->model, $this->resource, $this->options);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Registration $registration)
+    public function show(int $id)
     {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Registration $registration)
-    {
-        //
+        return showTemplate($this->model, $this->resource, $id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRegistrationRequest $request, Registration $registration)
+    public function update(Request $request, string $activity_id)
     {
-        //
+
+        return updateTemplate($request, $this->model, $activity_id, $this->resource, $this->options, $this->model_name);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Registration $registration)
+    public function destroy(int $id)
     {
-        //
+
+        return destroyTemplate($this->model, $id);
     }
 }
