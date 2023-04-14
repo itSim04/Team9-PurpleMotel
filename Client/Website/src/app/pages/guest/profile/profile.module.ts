09d45da7a99e5617e415018c11956725849fc86a
Charbellgg <charbelgerges40@gmail.com>
2023-04-14 13:11:06 +0300

    Updated the Profile Design🤷‍♂️
    1. Design, Implement and Link Profile
    2. 10
    3. Added the buttons needed along with the header.

--- Client/Website/src/app/pages/guest/profile/profile.module.ts
+++ Client/Website/src/app/pages/guest/profile/profile.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarModule } from 'src/app/components/general/nav-bar/nav-bar.module';
+import { FooterModule } from 'src/app/components/general/footer/footer.module';
+import { ProfileComponent } from './profile.component';
+import { LanguageModule } from 'src/app/services/language/language.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    NavBarModule,
+    FooterModule,
+    LanguageModule
   ]
 })
 export class ProfileModule { }
