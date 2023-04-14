060976e0b8bb763f015de587638f8b4675241302
Daniel Khater <formaldk737@gmail.com>
2023-04-14 20:56:57 +0300

    Created Supporting Files🛖
    1. Design, Implement Home Page and Link News Feed
    2. 10
    3. Created the supporting files for the news item component on the home page.

---
+++ Client/Website/src/app/components/news/news-list-item/news-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-list-item',
+  templateUrl: './news-list-item.component.html',
+  styleUrls: ['./news-list-item.component.scss']
+})
+export class NewsListItemComponent {
+
+}
---
+++ Client/Website/src/app/components/news/news-list-item/news-list-item.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsListItemComponent } from './news-list-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewsListItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewsListItemModule { }
