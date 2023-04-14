f43d23e09dd46b579773cc89debb7bbf7b15920a
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-14 12:57:53 +0300

    Created a basic component🍴
    1. Design, Implement and Link Restaurant Page
    2. 10
    3. Created a basic component to display food item in a list

---
+++ Client/Website/src/app/components/food/food-list-item/food-list-item.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { formatPrice } from '../../database/database.component';
+
+@Component({
+  selector: 'app-food-list-item',
+  templateUrl: './food-list-item.component.html',
+  styleUrls: ['./food-list-item.component.scss']
+})
+export class FoodListItemComponent {
+
+  @Input() name!: string;
+  @Input() description!: string;
+  @Input() price!: number;
+  @Input() quantity!: number;
+
+
+  get formatTotalPrice(): string {
+    
+    const numStr = (this.price * this.quantity).toString();
+
+    // split the number string into groups of three digits from right to left
+    const numArr = numStr.split('').reverse().join('').match(/(\d{1,3})/g);
+
+    // join the groups with commas and return the result from right to left
+    return numArr?.join(',')?.split('').reverse().join('') || numStr;
+    
+  }
+
+  get formatPrice(): string {
+    
+    return formatPrice(this.price);
+  }
+
+  changeQuantity(change: number) {
+
+    this.quantity += change;
+
+  }
+
+}
