47fe409aff09daeacd2c7fea04ba72177a955fed
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-14 12:43:12 +0300

    Fixed a few files🏡
    1. Design, Implement and Link Rooms Page
    2. 10
    3. Fixed a few files in accordance to conventions used by other branches

--- Client/Website/src/app/pages/guest/rooms/rooms.module.ts
+++ Client/Website/src/app/pages/guest/rooms/rooms.module.ts
@@ -1,3 +1,4 @@
+import { RoomOverviewComponent } from './overview/room-overview.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowseRoomsComponent } from './browse/browse-rooms.component';
@@ -8,7 +9,8 @@ import { NavBarModule } from 'src/app/components/general/nav-bar/nav-bar.module'
 
 @NgModule({
   declarations: [
-    BrowseRoomsComponent
+    BrowseRoomsComponent,
+    RoomOverviewComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import { NavBarModule } from 'src/app/components/general/nav-bar/nav-bar.module'
     FooterModule
   ],
   exports: [
-    BrowseRoomsComponent
+    BrowseRoomsComponent,
+    RoomOverviewComponent
   ]
 })
 export class RoomsModule { }
