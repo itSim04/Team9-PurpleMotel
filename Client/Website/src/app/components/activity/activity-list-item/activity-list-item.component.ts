6148154e851497afb99feae72a5efb1c9f31977e
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-14 21:33:48 +0300

    Added a Number of Seats Functionality🪂
    1. Design, Implement and Link Services Page
    2. 10
    3. Added number of seats and fixed a few things.

--- Client/Website/src/app/components/activity/activity-list-item/activity-list-item.component.ts
+++ Client/Website/src/app/components/activity/activity-list-item/activity-list-item.component.ts
@@ -10,33 +10,33 @@ import { formatPrice } from '../../database/database.component';
 })
 export class ActivityListItemComponent {
   @Input() activity?: KeyValue<string, Activity>;
-  @Input() title!: string;
-  @Input() description!: string;
-  @Input() capacity!: number;
-  @Input() price!: number;
-  //@Input() seats!: number;
-  @Input() start_date!: string;
-  @Input() end_date!: string;
+  // @Input() title!: string;
+  // @Input() description!: string;
+  // @Input() capacity!: number;
+  // @Input() price!: number;
+  @Input() seats: number=0;
+  // @Input() start_date!: string;
+  // @Input() end_date!: string;
 
-  get formatTotalPrice(): string {
-    const numStr = (this.price).toString();
+  // get formatTotalPrice(): string {
+  //   const numStr = (this.price).toString();
 
-    // split the number string into groups of three digits from right to left
-    const numArr = numStr.split('').reverse().join('').match(/(\d{1,3})/g);
+  //   // split the number string into groups of three digits from right to left
+  //   const numArr = numStr.split('').reverse().join('').match(/(\d{1,3})/g);
 
-    // join the groups with commas and return the result from right to left
-    return numArr?.join(',')?.split('').reverse().join('') || numStr;
-  }
-
+  //   // join the groups with commas and return the result from right to left
+  //   return numArr?.join(',')?.split('').reverse().join('') || numStr;
+  // }
+  
   get formatPrice(): string {
 
     return formatPrice(this.activity?.value.price);
 
   }
- /* changeQuantity(change: number) {
-
+  changeQuantity(change: number) {
+    if (this.seats + change >= 0) {
     this.seats += change;
-
-  }*/
+    }
+  }
 
 }
