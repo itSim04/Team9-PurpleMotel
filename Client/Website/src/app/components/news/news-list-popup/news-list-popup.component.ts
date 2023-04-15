e9b4938269f08558f2f0deec120c8adc5e720b37
Daniel Khater <formaldk737@gmail.com>
2023-04-15 18:43:54 +0300

    Created Supporting Files🛖
    1. Design, Implement Home Page and Link News Feed
    2. 9
    3. Crested the sporting files that will be the base for the news pop up menu.

---
+++ Client/Website/src/app/components/news/news-list-popup/news-list-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-list-popup',
+  templateUrl: './news-list-popup.component.html',
+  styleUrls: ['./news-list-popup.component.scss']
+})
+export class NewsListPopupComponent {
+
+}
