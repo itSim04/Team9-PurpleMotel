0a63167e5874dcd1eed27b94ffa0d408dfa0ec67
Charbellgg <charbelgerges40@gmail.com>
2023-04-15 11:57:20 +0300

    Added the Animations for the Carousel🤷‍♂️
    1. Design, Implement and Link Profile
    2. 10
    3. Added the animations for the carousel that will be added later on for the items.

--- Client/Website/src/app/pages/guest/profile/profile.component.ts
+++ Client/Website/src/app/pages/guest/profile/profile.component.ts
@@ -1,9 +1,17 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  animations: [
+    trigger('fadeOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 })),
+      ])])]
+
 })
 export class ProfileComponent {
 
