e7d5844140f95099c83d3292140e839d291b43ad
Elite Keryakos <elite.keryakos@hotmail.com>
2023-04-15 20:48:59 +0300

    Implemented Edit Profile Logic🤷‍♂️
    1. Design, Implement and Link Profile
    2. 10
    3. Implemented the logic.

--- Client/Website/src/app/pages/guest/profile/profile.service.ts
+++ Client/Website/src/app/pages/guest/profile/profile.service.ts
@@ -1,17 +1,20 @@
 import { ComponentType } from '@angular/cdk/portal';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from 'src/app/services/dialogs/authentication/login/login.component';
 import { RegisterComponent } from 'src/app/services/dialogs/authentication/register/register.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { User, UserInformation, UserResponse } from 'src/app/models/User';
+import { Observable } from 'rxjs';
+import { UserDatabaseService } from '../../admin/user-database/user-database.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileDialogService {
 
-  constructor(public dialog: MatDialog, private request: HttpClient) { }
+  constructor(public dialog: MatDialog, private request: HttpClient, private userDatabaseService: UserDatabaseService) { }
 
   openDialog(type: 'edit_profile' | 'change_password') {
 
@@ -33,4 +36,42 @@ export class ProfileDialogService {
 
     return this.dialog.open(component, {});
   }
+
+  edit_profile(
+    email: string,
+    first_name: string,
+    last_name: string,
+    date_of_birth: string,
+    phone: string,
+    gender: string,
+
+  ) {
+    const user_id = localStorage.getItem('id');
+    const tier = (JSON.parse(localStorage.getItem('user')|| '{}') as User).tier;
+    const type = (JSON.parse(localStorage.getItem('user')|| '{}') as User).type;
+    const language = (JSON.parse(localStorage.getItem('user')|| '{}') as User).language;
+
+
+    if (!user_id) {
+      throw new Error('User ID not found');
+    }
+  
+    const updatedUser: User = {
+      tier,
+      language,
+      type,
+      email,
+      first_name,
+      last_name,
+      date_of_birth,
+      phone,
+      gender,
+      permissions: new Map<string, number>()
+
+    };
+  
+    return this.userDatabaseService.modifyUser(user_id, updatedUser);
+  }
+  
+
 }
