9fe0eef88534b86a71688e6351fb5e617a653bfc
Charbellgg <charbelgerges40@gmail.com>
2023-04-22 18:23:45 +0300

    Added Guards to the Application📱
    1. N/A
    2. 10
    3. Added guards so that a user cannot access specific routes without authentication and being logged in.

---
+++ Client/Application/src/app/guards/guest.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { extractUser } from '../components/database/database.component';
+import { AuthenticationDialogService } from '../authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestGuard implements CanActivate {
+
+  constructor (private router: Router, private authentication: AuthenticationDialogService) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const user = extractUser();
+    if (user) {
+      return true;
+    } else {
+
+      this.router.navigate(['/']);
+      this.authentication.openDialog('login');
+      return false;
+
+    }
+  }
+}
