c9264efc18084c20ebef3f50ee300c15a45f58c5
Charbellgg <charbelgerges40@gmail.com>
2023-04-27 14:25:53 +0300

    Updated the Component with Useful Methods🏁
    1. Design, Implement and Link Services Page
    2. 10
    3. Modified the file with methods such as format price, change quantity.

--- Client/Application/src/app/pages/guest/profile/profile-modal/profile-modal.component.ts
+++ Client/Application/src/app/pages/guest/profile/profile-modal/profile-modal.component.ts
@@ -1,9 +1,6 @@
-import { KeyValue } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NavParams, ModalController } from '@ionic/angular';
-import { Interface } from 'readline';
-import { Required } from 'src/app/components/database/database.component';
-import { Activity } from 'src/app/models/Activity';
+import { Required, formatPrice } from 'src/app/components/database/database.component';
+
 export interface ProfileModalData {
 
   title?: string;
@@ -13,14 +10,14 @@ export interface ProfileModalData {
   end_date?: string;
   image?: string;
   button?: {
-    
+
     label: string;
     action: (...args: unknown[]) => void;
-    
-  }
+
+  
+  };
   stored_data?: unknown;
   show_num_input?: boolean;
-
 }
 
 @Component({
@@ -28,6 +25,7 @@ export interface ProfileModalData {
   templateUrl: './profile-modal.component.html',
   styleUrls: ['./profile-modal.component.scss'],
 })
+
 export class ProfileModalComponent {
 
   @Input() @Required data?: ProfileModalData
@@ -39,6 +37,32 @@ export class ProfileModalComponent {
       this.data.button.action(this.data.stored_data);
 
 
+  }
+  changeQuantity(change: number) {
+
+    if (this.data) {
+      let seats = this.data.stored_data as number;
+
+      if (seats + change >= 0) {
+        seats += change;
+      }
+    }
+
+  }
+
+  get formatTotalPrice(): string {
+    if (this.data) {
+      if(this.data.price && this.data.stored_data){
+        const seats = this.data.stored_data as number;
+        const totalPrice = this.data.price * seats;
+        return formatPrice(totalPrice);
+      } 
+      else {
+        return '';
+      }
+
+    }
+    return '';
   }
 
 
