eb446fde8826133930a190b1bf76b6bdecf8b0d0
Charbellgg <charbelgerges40@gmail.com>
2023-04-28 21:29:21 +0300

    Populated Like News Policy🏁
    1. N/A
    2. 10
    3. Implemented the like news policy.

--- Server/app/Policies/LikesNewsPolicy.php
+++ Server/app/Policies/LikesNewsPolicy.php
@@ -1,66 +1,101 @@
 <?php
 
 namespace App\Policies;
-
-use App\Models\LikesNews;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
-class LikesNewsPolicy
+class LikeNewsPolicy
 {
+
+    private $permission_name = 'like-news';
+
     /**
-     * Determine whether the user can view any models.
+     * Create a new policy instance.
      */
-    public function viewAny(User $user): bool
+    public function __construct()
     {
         //
     }
 
     /**
      * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
      */
-    public function view(User $user, LikesNews $likesNews): bool
+    public function viewAny(User $user)
     {
-        //
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
+
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][0];
+
+        }
     }
 
     /**
-     * Determine whether the user can create models.
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user_type
+     * @return bool
      */
-    public function create(User $user): bool
+    public function view(User $user): bool
     {
-        //
+
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
+
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][0];
+            
+        }
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, LikesNews $likesNews): bool
-    {
-        //
+    public function update(User $user) {
+
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
+
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][1];
+            
+        }
+
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, LikesNews $likesNews): bool
-    {
-        //
-    }
+    public function delete(User $user) {
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, LikesNews $likesNews): bool
-    {
-        //
-    }
+        if($user->tier == 2) {
+            return true;
+        }
+        $permissions = extractPermissions($user->id, $user->type);
+        if (!array_key_exists($this->permission_name, $permissions)) {
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, LikesNews $likesNews): bool
-    {
-        //
+            return false;
+
+        } else {
+
+            return $permissions[$this->permission_name][2];
+            
+        }
+
     }
 }
