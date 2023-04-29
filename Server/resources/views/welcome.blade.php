e39991760d52b0b8e796c5e25e01b3ceff1ce7c7
Charbellgg <charbelgerges40@gmail.com>
2023-04-29 19:58:30 +0300

    Documented RoomType Store🏁
    1. N/A
    2. 10
    3. N/A

--- Server/resources/views/welcome.blade.php
+++ Server/resources/views/welcome.blade.php
@@ -774,6 +774,97 @@
 
             </div>
         </div>
+        <div>
+            <h2 style="font-size: 4vw;">1.2 store</h2>
+            <h3>Post</h3>
+            <h3>http://example.com/api/v1/room_types</h3>
+
+            <div class="card mb-3">
+                <div class="card-header">
+                    <h4 style="background-color: #80808041;padding:16px">Parameters</h4>
+                </div>
+                <div class="card-body">
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Name</td>
+                            <td class="table-col">Type</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$request</td>
+                            <td class="table-content">Request</td>
+                            <td class="table-content">Used to validate the attributes passed to it and to fetch needed attributes.</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$model</td>
+                            <td class="table-content">string</td>
+                            <td class="table-content">Specifies the type of model passed to the function. Will be used to create an instance of this model.</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$resource</td>
+                            <td class="table-content">string</td>
+                            <td class="table-content">Specifies the type of resource passed to the function. Will be used to create an instance of this resource.</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$options</td>
+                            <td class="table-content">array</td>
+                            <td class="table-content">Holds the room type attributes.</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$singular</td>
+                            <td class="table-content">bool</td>
+                            <td class="table-content">Specifies if the function should return a single resource or a collection of resources. Returns a single resource by default.</td>
+                        </tr>
+
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Description:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">This functions takes as input the attributes of a room type and creates an instance of a room type in the database using these attributes, along with a resource. </td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Returns:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Type</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">JSON</td>
+                            <td class="table-content">A JSON object potraying the room type resource. This will result in a 201 status code if successful.</td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Errors:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Error</td>
+                            <td class="table-desc">Description</td>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Unauthenticated</td>
+                            <td class="table-content">If the user trying to add the room does not have the required permissions.</td>
+
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Internal Server Error</td>
+                            <td class="table-content">If the connection is lost or if the server crashes.</td>
+                        </tr>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+        <div>
+
+        </div>
 
 
     </div>
