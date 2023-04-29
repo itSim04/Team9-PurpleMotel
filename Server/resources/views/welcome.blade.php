3e16230de46378dbd128f47d203ad63b0b682b2d
Charbellgg <charbelgerges40@gmail.com>
2023-04-29 19:46:52 +0300

    Documented Room postReview🏁
    1. N/A
    2. 10
    3. N/A

--- Server/resources/views/welcome.blade.php
+++ Server/resources/views/welcome.blade.php
@@ -254,7 +254,7 @@
 
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">$id</td>
-                            <td class="table-content">int</td> 
+                            <td class="table-content">int</td>
                             <td class="table-content">The id specifices which room to fetch.</td>
                         </tr>
 
@@ -291,7 +291,7 @@
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">{id} not in Database</td>
                             <td class="table-content">If the room does not exist</td>
-                           
+
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Unauthenticated</td>
@@ -390,7 +390,7 @@
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">{id} not in Database</td>
                             <td class="table-content">If the room does not exist</td>
-                           
+
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Unauthenticated</td>
@@ -432,7 +432,7 @@
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">$id</td>
-                            <td class="table-content">int</td> 
+                            <td class="table-content">int</td>
                             <td class="table-content">The id specifices which room to delete.</td>
                         </tr>
                         <tr style="height:8px;text-align:center;">
@@ -489,7 +489,7 @@
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">{id} not in Database</td>
                             <td class="table-content">If the room does not exist</td>
-                           
+
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Unauthenticated</td>
@@ -559,6 +559,7 @@
                             <td class="table-col">Error</td>
                             <td class="table-desc">Description</td>
                         </tr>
+                        
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Internal Server Error</td>
                             <td class="table-content">If the connection is lost or if the server crashes.</td>
@@ -621,6 +622,77 @@
                         <tr style="height:8px;text-align:center;">
                             <td class="table-col">Error</td>
                             <td class="table-desc">Description</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Unauthorized</td>
+                            <td class="table-content">If the user is not a guest.</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Internal Server Error</td>
+                            <td class="table-content">If the connection is lost or if the server crashes.</td>
+                        </tr>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+        <div>
+            <h2 style="font-size: 4vw;">1.8 postReview</h2>
+            <h3>Post</h3>
+            <h3>http://example.com/api/v1/rooms/</h3>
+
+            <div class="card mb-3">
+                <div class="card-header">
+                    <h4 style="background-color: #80808041;padding:16px">Parameters</h4>
+                </div>
+                <div class="card-body">
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Name</td>
+                            <td class="table-col">Type</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$request</td>
+                            <td class="table-content">Request</td>
+                            <td class="table-content">Used to hold and validate the room attributes provied by the user.</td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Description:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">This function request containing information about a review, validates the request data, and then either creates a new review in the database or returns an error message indicating that the user has already posted a review for that room. If a review is found, the function returns a 400 error with a message indicating that the user has already posted a review. If no review is found, the function creates a new review in the database using the data from the request. The new review is then returned in a 200 response.</td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Returns:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Type</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">JSON</td>
+                            <td class="table-content"> Returns a JSON object containing the review that the user posted along with a 200 status if successful. If the user has already reviewed the room, a 400 status is returned along with an informative message.</td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Errors:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Error</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Unauthorized</td>
+                            <td class="table-content">If the user is not a guest.</td>
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Internal Server Error</td>
