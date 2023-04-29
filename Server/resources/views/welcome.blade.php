976441182bec41706f42e682e26d5be22649dd48
Charbellgg <charbelgerges40@gmail.com>
2023-04-29 21:01:55 +0300

    Documented RoomType Destroy🏁
    1. N/A
    2. 10
    3. N/A

--- Server/resources/views/welcome.blade.php
+++ Server/resources/views/welcome.blade.php
@@ -403,7 +403,7 @@
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Unauthenticated</td>
-                            <td class="table-content">If the user trying to add the room does not have the required permissions.</td>
+                            <td class="table-content">If the user trying to modify the room does not have the required permissions.</td>
 
                         </tr>
                         <tr style="height:8px;text-align:center;">
@@ -476,7 +476,7 @@
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Unauthenticated</td>
-                            <td class="table-content">If the user trying to add the room does not have the required permissions.</td>
+                            <td class="table-content">If the user trying to delete the room does not have the required permissions.</td>
 
                         </tr>
                         <tr style="height:8px;text-align:center;">
@@ -1051,11 +1051,84 @@
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">Unauthenticated</td>
-                            <td class="table-content">If the user trying to add the room type does not have the required permissions.</td>
+                            <td class="table-content">If the user trying to modify the room type does not have the required permissions.</td>
 
                         </tr>
                         <tr style="height:8px;text-align:center;">
                             <td class="table-content">An error occured</td>
+                            <td class="table-content">If the connection is lost or if the server crashes.</td>
+                        </tr>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+        <div>
+            <h2 style="font-size: 4vw;">1.5 destroy</h2>
+            <h3>Delete</h3>
+            <h3>http://example.com/api/v1/room_types/{id}</h3>
+
+            <div class="card mb-3">
+                <div class="card-header">
+                    <h4 style="background-color: #80808041;padding:16px">Parameters</h4>
+                </div>
+                <div class="card-body">
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Name</td>
+                            <td class="table-col">Type</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">$id</td>
+                            <td class="table-content">int</td>
+                            <td class="table-content">The id specifices which room type to delete.</td>
+                        </tr>
+                       
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Description:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">The purpose of the function is to delete a room type from the database using the specified model and ID. If the $safety_check parameter is provided, the function will perform a safety check to ensure that the record can be safely deleted. The safety check involves finding related records in the $safety_check model using the foreign key and primary key columns. If related records are found, the function will return a response containing a collection of related records using the specified $safety_resource resource. If no related records are found, the function will proceed with deleting the record using the specified model and ID. If the delete operation is successful, the function will return a response indicating success. If the delete operation fails, the function will return a response indicating that the specified ID was not found in the database.</td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Returns:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Type</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">JSON</td>
+                            <td class="table-content"> Will result in a 200 status code if no related records were found. Returns a 200 status code along with the related records otherwise. </td>
+                        </tr>
+
+                    </table>
+                    <h4 style="background-color: #80808041;padding:16px">Errors:</h4>
+                    <table style="table-layout: auto;width:100%">
+
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-col">Error</td>
+                            <td class="table-desc">Description</td>
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">{id} not in Database</td>
+                            <td class="table-content">If the room type does not exist</td>
+
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Unauthenticated</td>
+                            <td class="table-content">If the user trying to delete the room type does not have the required permissions.</td>
+
+                        </tr>
+                        <tr style="height:8px;text-align:center;">
+                            <td class="table-content">Internal Server Error</td>
                             <td class="table-content">If the connection is lost or if the server crashes.</td>
                         </tr>
 
