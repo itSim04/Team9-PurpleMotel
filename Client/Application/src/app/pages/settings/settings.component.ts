81f1806823f5d76b01b90bf06cbac4f698f675e4
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-30 14:02:28 +0300

    Added functionality to promo codes🏁
    1. N/A
    2. 10
    3. Added full functionality to promo codes

--- Client/Application/src/app/pages/settings/settings.component.ts
+++ Client/Application/src/app/pages/settings/settings.component.ts
@@ -1,4 +1,6 @@
-import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { PromoDatabaseService } from 'src/app/services/providers/promo-database.service';
+import { Platform, ToastController } from '@ionic/angular';
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 
@@ -10,13 +12,14 @@ import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 export class SettingsComponent implements OnInit {
 
   theme = '';
-  constructor (@Inject(DOCUMENT) private document: Document, private platform: Platform) { }
+  promo = '';
+  constructor (@Inject(DOCUMENT) private document: Document, private platform: Platform, private toastController: ToastController, private promo_service: PromoDatabaseService, private router: Router) { }
 
   ngOnInit() {
 
     this.theme = localStorage.getItem('theme') || 'system';
 
-   }
+  }
 
   // async ionViewDidEnter() {
   //   await this.platform.ready().then(() => {
@@ -57,5 +60,60 @@ export class SettingsComponent implements OnInit {
         }
     }
   }
+
+  applyPromo() {
+
+    this.promo_service.applyPromoCode(this.promo).subscribe(response => {
+
+      switch (response) {
+
+        case 200:
+
+          this.displayToast('Promo code already registered');
+          break;
+
+        case 201:
+
+
+          this.displayToast('Promo code successfully registered');
+          break;
+
+        case 400:
+
+          this.displayToast('You already have a promo code');
+          break;
+
+        case 403:
+
+          localStorage.removeItem('token');
+          localStorage.removeItem('user');
+          localStorage.removeItem('id');
+          localStorage.removeItem('token_time');
+          this.router.navigate(['/auth']);
+          break;
+
+        case 404:
+
+          this.displayToast('Invalid Promo Code');
+          break;
+
+      }
+
+    });
+
+  }
+  async displayToast(body: string) {
+
+    const toast = await this.toastController.create({
+      message: body,
+      duration: 1500,
+      position: 'bottom'
+    });
+
+    await toast.present();
+
+  }
+
+
 
 }
