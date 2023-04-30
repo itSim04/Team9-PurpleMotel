50e742a10e1a43cd7877e69209db59549e5755ae
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-04-30 22:02:24 +0300

    Prepared the intel table
    1. N/A
    2. 10
    3. Prepared a table to record guest preferences

---
+++ Server/app/Http/Controllers/IntelController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Intel;
+use App\Http\Requests\StoreIntelRequest;
+use App\Http\Requests\UpdateIntelRequest;
+
+class IntelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIntelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Intel $intel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Intel $intel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIntelRequest $request, Intel $intel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Intel $intel)
+    {
+        //
+    }
+}
---
+++ Server/app/Models/Intel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Intel extends Model
+{
+    use HasFactory;
+}
