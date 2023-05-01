85ad342285c497f24de7d2e4b37d07cf25f4659c
Charbellgg <charbelgerges40@gmail.com>
2023-05-01 13:14:54 +0300

    Cleaned Fourth Chunk of the Website🏁
    1. N/A
    2. 10
    3. Removed comments, unused variables and imports, along with sorting variables and methods.

--- Client/Website/src/app/pages/guest/restaurant/landing/landing-restaurant.component.ts
+++ Client/Website/src/app/pages/guest/restaurant/landing/landing-restaurant.component.ts
@@ -1,5 +1,4 @@
 import { UrlBuilderService } from './../../../../services/utility/url-builder.service';
-import { ImageCachingService } from './../../../../services/utility/image-caching.service';
 import { Component } from '@angular/core';
 import { DisplayDialogService } from 'src/app/services/utility/display.service';
 
